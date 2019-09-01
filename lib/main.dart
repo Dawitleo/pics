1cd83805b0ca1d020367b2019cc75c86eaf14fc2
Dawit <dawitgebeta@gmail.com>
2019-09-01 16:43:47 +0300

    Fetch image with http

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
-import 'dart:convert';
 
 void main() {
   
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
 
@@ -10,17 +13,20 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() async{
+    counter++;
+    var response = await get('http://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    
+  }
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Text("$counter"),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: (){
-            setState(() {
-              counter++;
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text("Lets see some images!"),
