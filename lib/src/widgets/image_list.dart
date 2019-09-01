d0cfab0df11efc41db882a8221f8b135a13f06e4
Dawit <dawitgebeta@gmail.com>
2019-09-01 17:25:00 +0300

    Fetch image and display image in container

---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> images;
+  ImageList(this.images);
+
+  Widget build(context){
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index){
+        return Container(
+          margin: EdgeInsets.all(20.0),
+          child: Image.network(images[index].url),
+        );
+      },
+    );
+  }
+}
